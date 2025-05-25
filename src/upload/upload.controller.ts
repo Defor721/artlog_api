f3550a39b3cc2463eb9ecdaa43a40c79a33912fc
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-05-25 21:10:57 +0900

    [docs] upload controller에 swagger 태그 추가

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { S3Service } from '../s3/s3.service';
-
+import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
 @Controller('upload')
 export class UploadController {
   constructor(private readonly s3Service: S3Service) {}
 
   @Get('presigned-url')
+  @ApiOperation({ summary: '사진 업로드' })
+  @ApiResponse({
+    status: 200,
+    description: '업로드 url, 접근용 publicurl 반환',
+  })
   async getPresignedUrl(
     @Query('fileName') fileName: string,
     @Query('fileType') fileType: string,
