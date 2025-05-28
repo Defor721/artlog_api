e6c2465b91d7b35b9ff45e790332b8eb0f350971
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-05-28 19:50:33 +0900

    [Feat] 댓글 수정 기능 추가, db에 수정 이력 저장하도록 함

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 
@@ -28,6 +32,33 @@ export class CommentsService {
         author: { select: { id: true, name: true, image: true } },
       },
       orderBy: { createdAt: 'asc' },
+    });
+  }
+
+  async updateComment(commentId: string, newContent: string, userId: string) {
+    const comment = await this.prisma.comment.findUnique({
+      where: { id: commentId },
+    });
+    if (!comment) throw new NotFoundException('댓글을 찾을 수 없습니다');
+    if (comment.authorId !== userId)
+      throw new ForbiddenException('수정 권한이 없습니다');
+
+    const updatedHistory = Array.isArray(comment.editHistory) // prisma는 mongodb 기준으로 배열 타입을 지정할 수 없음. json타입으로 다뤄야 하기 때문에 TS쪽에서 방어 코드를 넣어줘야 함.
+      ? [...comment.editHistory]
+      : [];
+
+    updatedHistory.push({
+      previousContent: comment.content,
+      editedAt: new Date().toISOString(),
+    });
+
+    return this.prisma.comment.update({
+      where: { id: commentId },
+      data: {
+        content: newContent,
+        edited: true,
+        editHistory: updatedHistory,
+      },
     });
   }
 }
