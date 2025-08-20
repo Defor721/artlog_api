0d40f909c35ba30607fc36d6b2311313e461d014
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-08-20 19:47:16 +0900

    [Feat] appmodule에 throttler 관련 옵션 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,10 +14,10 @@ import { LoggerModule } from './common/logger/logger.module';
 import { S3Service } from './s3/s3.service';
 import { ConfigModule } from '@nestjs/config';
 import { CommentsModule } from './comments/comments.module';
-
 import { DiarysModule } from './diarys/diarys.module';
 import { AdminModule } from './admin/admin.module';
 import { HealthModule } from './health/health.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -33,10 +33,21 @@ import { HealthModule } from './health/health.module';
     DiarysModule,
     AdminModule,
     HealthModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60, // 60초 동안
+          limit: 100, // 최대 100회 요청 가능
+        },
+      ],
+    }),
   ],
   controllers: [AppController, AuthController],
   providers: [
     AppService,
+    // ✅ 전역 레이트 리밋 가드 (먼저 실행되도록 위에 배치)
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    // ✅ 그 다음 권한 가드
     { provide: APP_GUARD, useClass: RolesGuard },
     S3Service,
   ],
